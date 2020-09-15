34a1cd9842f3f74d630febc19dc9587de3fae496
ThireshL <thiresh93@gmx.de>
2020-09-15 20:17:09 +0200

    Intian commit

---
+++ src/visualization/visualize_dashboard_1.py
@@ -0,0 +1,124 @@
+import numpy as np
+import pandas as pd
+import dash
+dash.__version__
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output,State
+
+import plotly.graph_objects as go
+
+import os
+print(os.getcwd())
+df_input_large=pd.read_csv('C:/Users/thire/Desktop/eds_lodaya_406410_covid_19/data/processed/COVID_final_set.csv',sep=';')
+
+fig = go.Figure()
+
+app = dash.Dash()
+app.layout = html.Div([
+
+    dcc.Markdown('''
+    #  Applied Data Science on COVID-19 Data - Part 1
+
+    * Goal of the project is to learn data science by applying a cross-industry standard process.\
+        The default layout contains the confirmed infected cases in the log-scale format on the Y-axis\
+        and Timeline in Days on the X-axis.
+
+    ### The first dropdown menu enables selection of one or multiple  countries for visualization.
+    ### The seconds dropdown menu contains four option:
+        1. The ‘Timeline Confirmed’ represents confirmed infected cases along the timeline.
+        2. The ‘Timeline Confirmed Filtered’ represents filtered(savgol filter) confirmed infected cases along the timeline.
+        3. The ‘Timeline Doubling Rate’ represents the doubling rate of the infected cases along the timeline.
+        4. The ‘Timeline Doubling Rate Filtered’ represents the doubling rate of the filtered(savgol filter) infected cases along the timeline.
+
+    '''),
+
+    dcc.Markdown('''
+    ### Select country below:
+    '''),
+
+
+    dcc.Dropdown(
+        id='country_drop_down',
+        options=[ {'label': each,'value':each} for each in df_input_large['country'].unique()],
+        value=['US', 'India','Brazil'], # which are pre-selected
+        multi=True
+    ),
+
+    dcc.Markdown('''
+        ### Select timeline of confirmed COVID-19 cases or the approximated doubling time:
+        '''),
+
+
+    dcc.Dropdown(
+    id='doubling_time',
+    options=[
+        {'label': 'Timeline Confirmed ', 'value': 'confirmed'},
+        {'label': 'Timeline Confirmed Filtered', 'value': 'confirmed_filtered'},
+        {'label': 'Timeline Doubling Rate', 'value': 'confirmed_DR'},
+        {'label': 'Timeline Doubling Rate Filtered', 'value': 'confirmed_filtered_DR'},
+    ],
+    value='confirmed',
+    multi=False
+    ),
+
+    dcc.Graph(figure=fig, id='main_window_slope')
+])
+
+
+
+@app.callback(
+    Output('main_window_slope', 'figure'),
+    [Input('country_drop_down', 'value'),
+    Input('doubling_time', 'value')])
+def update_figure(country_list,show_doubling):
+
+
+    if 'DR' in show_doubling:
+        my_yaxis={'type':"log",
+               'title':'Approximated doubling rate over 3 days (larger numbers are better #stayathome)'
+              }
+    else:
+        my_yaxis={'type':"log",
+                  'title':'Confirmed infected people (source johns hopkins csse, log-scale)'
+              }
+
+
+    traces = []
+    for each in country_list:
+
+        df_plot=df_input_large[df_input_large['country']==each]
+
+        if show_doubling=='doubling_rate_filtered':
+            df_plot=df_plot[['state','country','confirmed','confirmed_filtered','confirmed_DR','confirmed_filtered_DR','date']].groupby(['country','date']).agg(np.mean).reset_index()
+        else:
+            df_plot=df_plot[['state','country','confirmed','confirmed_filtered','confirmed_DR','confirmed_filtered_DR','date']].groupby(['country','date']).agg(np.sum).reset_index()
+
+
+        traces.append(dict(x=df_plot.date,
+                                y=df_plot[show_doubling],
+                                mode='markers+lines',
+                                opacity=0.9,
+                                name=each
+                        )
+                )
+
+    return {
+            'data': traces,
+            'layout': dict (
+                width=1280,
+                height=720,
+
+                xaxis={'title':'Timeline',
+                        'tickangle':-45,
+                        'nticks':20,
+                        'tickfont':dict(size=14,color="#7f7f7f"),
+                      },
+
+                yaxis=my_yaxis
+        )
+    }
+
+if __name__ == '__main__':
+
+    app.run_server(debug=True, use_reloader=False)
